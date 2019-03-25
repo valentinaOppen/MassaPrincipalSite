906a3d19f98f8af40f46f24684122d6b7e365f64
elias alvarez <ealvarez@cloud.studio>
2019-03-25 01:21:42 -0300

    Fix parallax responsive Desktop-tablet

---
+++ js/main.js
@@ -0,0 +1,24 @@
+$(document).ready(function() {
+    $("#divFormulario").on("submit", function(e) {
+        e.preventDefault();
+        var theBtn = $('#btnEnviar');
+        if ( $('#btnEnviar').is('.disabled')) {
+            theBtn.removeClass('disabled'); // enable the button?
+            lightFill(); //show fill inputs
+            lightEmpty(); //show empty inputs
+            return false;
+        }
+
+        theBtn.addClass('disabled')  
+            var data = $(this).serialize();
+            $.ajax({
+            url: 'mail.php',
+            type: 'POST',
+            data: data,
+        }).done(function(data) {
+            //some code going here if success 
+        }).fail(function() {
+            theBut.removeClass('disabled')
+        });
+    });
+});
